e0232f307d941a75ea1a6f89fc4942f4670cfde3
Shaun Swinehart <shaun.leroyhigh@hotmail.com>
2023-09-27 22:37:50 -0500

    added ability to find all products, products by id, update product data, delete product data, and create new product data

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,6 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2), // Example: 10.99
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      validate: {
+        isInt: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
     // define columns
   },
   {
